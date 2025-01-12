30a1f4dcc09f3d8655d06f80da4a719d9aa8e144
midaszfg <platinum.nick6@gmail.com>
2025-01-12 19:36:39 +0100

    kk

---
+++ src/main/java/pl/vistula/_5/first_project_61215/FirstJavaSpringProject61215Application.java
@@ -0,0 +1,13 @@
+package pl.vistula._5.first_project_61215;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringProject61215Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringProject61215Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/vistula/_5/first_project_61215/FirstJavaSpringProject61215ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vistula._5.first_project_61215;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringProject61215ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
